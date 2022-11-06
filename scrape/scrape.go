818254a57702cef0105118f3c4efd3e8ef07d913
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-11-06 00:09:17 -0400

    Cleaning up redundant code

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -7,17 +7,23 @@ import (
 	"strings"
 )
 
+func HandleGetRequest(url string, r *http.Response, err error) {
+	if err != nil {
+		panic(err)
+	}
+	if r.StatusCode != 200 {
+		log.Fatalf("odd response status code: %v", r.StatusCode)
+	}
+}
+
 var bbrefPrefix = "https://baseball-reference.com"
 
 func FindYrBB(year string) string {
 	var def = "https://baseball-reference.com/leagues/"
 	r, err := http.Get(def)
-	if err != nil {
-		log.Fatalf("Error Getting baseball reference url: %v", err)
-	}
-	if r.StatusCode != 200 {
-		log.Fatalf("Failed to retrieve data: status code %v", r.StatusCode)
-	}
+	HandleGetRequest(def, r, err)
+
+	defer r.Body.Close()
 	doc, err := goquery.NewDocumentFromReader(r.Body)
 	if err != nil {
 		panic(err)
@@ -33,4 +39,13 @@ func FindYrBB(year string) string {
 		})
 	})
 	return yrHref
+}
+
+// we could probably make a get document function to split up the code more
+func FindTeamBB(yearHref string, team string) string {
+	r, err := http.Get(yearHref)
+	HandleGetRequest(yearHref, r, err)
+
+	defer r.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(r.Body)
 }
