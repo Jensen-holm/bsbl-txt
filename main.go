8ec93d747ab8b568e953dc928210820a10990f4b
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-11-05 21:49:32 -0400

    Getting request info back from scrape function

--- main.go
+++ main.go
@@ -1,6 +1,39 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"github.com/Jensen-holm/SportSimulation/scrape"
+	"os"
+	"strings"
+)
+
+func CLInput() string {
+	fmt.Println("Enter Team: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return ""
+	}
+	return input
+}
+
+type Team struct {
+	name string
+	year string
+}
 
 func main() {
 
+	ts := make([]Team, 0)
+
+	for i := 0; i < 2; i++ {
+		t := strings.Split(CLInput(), " ")
+		ts = append(ts, Team{
+			name: strings.Join(t[1:], " "),
+			year: t[0],
+		})
+	}
+
+	scrape.FindTeamBB(ts[0].name, ts[0].year)
 }
--- scrape/scrape.go
+++ scrape/scrape.go
@@ -1,5 +1,16 @@
 package scrape
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func FindTeamBB(team string, year string) {
-	
+	var def = "https://baseball-reference.com/leagues/"
+	html, err := http.Get(def)
+	if err != nil {
+		log.Fatalf("Error Getting baseball reference url: %v", err)
+	}
+	fmt.Println(html)
 }
