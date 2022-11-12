6e9666e0a0f404e8a4008fc44e54fd22bfe30efb
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-11-12 15:29:34 -0500

    Built the basic template of what it will look like when we start building the application, now moving on to baseball savant backend

--- server.go
+++ server.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
 	})
-
-	fmt.Printf("http://localhost:8080/")
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+	err := r.Run()
+	if err != nil {
+		panic(err)
 	}
 }
