a0525cd69f4fe0a95ec9d4db6824aef03d93375b
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-11-15 13:21:24 -0500

    db init commit

---
+++ baseball/db/db.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "jensen"
+	password = "ztcaxCb1"
+	dbname   = "statcast"
+)
+
+func Connect2DB() error {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Successfully connected!")
+	return nil
+}
--- baseball/go.mod
+++ baseball/go.mod
@@ -5,7 +5,10 @@ go 1.19
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
 	golang.org/x/text v0.4.0
-	github.com/andybalholm/cascadia v1.3.1 // indirect
-	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
 )
 
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
