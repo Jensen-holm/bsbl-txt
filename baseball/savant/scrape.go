0bc57be316d4b1759a4dfafce31814822f9449c2
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-11-15 10:02:39 -0500

    Skeleton of the id map funciton

--- baseball/savant/scrape.go
+++ baseball/savant/scrape.go
@@ -1,7 +1,6 @@
 package savant
 
 import (
-	"fmt"
 	"github.com/Jensen-holm/SportSimulation/scrape"
 	"github.com/PuerkitoBio/goquery"
 	"net/http"
@@ -19,9 +18,19 @@ func TmIdMap() (map[string]string, error) {
 		return map[string]string{}, err
 	}
 
-	tms := doc.Find("select#ddlteam")
-	fmt.Println(tms.Text())
-	return map[string]string{}, nil
+	tms := make(map[string]string, 0)
+	doc.Find("div .team-selector").Each(func(i int, selection *goquery.Selection) {
+		selection.Find("select .team-nav").Each(func(i int, selection *goquery.Selection) {
+			if id, ok := selection.Attr("id"); ok && id == "ddlteam" {
+				tm := selection.Find("option").Text()
+				val, ok := selection.Attr("value")
+				if ok {
+					tms[tm] = val
+				}
+			}
+		})
+	})
+	return tms, nil
 }
 
 func FindTeam(tm string, yr string) (string, error) {
