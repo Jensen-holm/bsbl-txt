c0680e54d464590d36fe4184091dc6fe5fa6c655
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-15 21:02:54 -0500

    cleaned up main function

--- main.go
+++ main.go
@@ -3,45 +3,29 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/Jensen-holm/SportSimulation/bbref"
+	"log"
 	"os"
 	"strings"
 
-	"github.com/Jensen-holm/SportSimulation/bbref"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 )
 
 func main() {
 
-	ts := make([]*bbref.Team, 0)
-
-	c := cases.Title(language.AmericanEnglish)
-
-	// create team structs
-	for i := 0; i < 2; i++ {
-		t := strings.Split(CLInput(), " ")
-		name := c.String(strings.Join(t[1:], " "))
-		yr := t[0]
-		ts = append(ts, bbref.NewTeam(name, yr))
+	teams, err := GetInput()
+	if err != nil {
+		log.Fatalf("error getting team input: %v", err)
 	}
 
-	bbref.GetTeams(ts)
-
-	for _, tm := range ts {
-		tm.EstimateRotation()
-		tm.EstimateLineup()
-		fmt.Println(tm.Hitters()[0])
-		//	// the estimate lineup situation when trying to consider
-		//	// position is tricky
-		//	// but I think we want users to set lineups
-		//	// or we scrape the lineups before each game anyway
-		//	// link - > https://www.lineups.com/mlb/lineups
-	}
+	bbref.GetTeams(teams)
+	TeamSetUp(teams)
 
 }
 
-// make a simulation func
-
+// CLInput -> prompts for and scans user input
+// for baseball teams to simulate against each other
 func CLInput() string {
 	fmt.Println("Enter Team: ")
 	reader := bufio.NewReader(os.Stdin)
@@ -50,4 +34,29 @@ func CLInput() string {
 		return ""
 	}
 	return strings.Replace(input, "\n", "", 1)
+}
+
+// GetInput -> Takes raw user input from CLI and creates
+// a baseball reference team object out of it. need to add
+// a check to see if they are real teams
+func GetInput() ([]*bbref.Team, error) {
+	var c = cases.Title(language.AmericanEnglish)
+
+	tms := make([]*bbref.Team, 0)
+	for i := 0; i < 2; i++ {
+		t := strings.Split(CLInput(), " ")
+		name := c.String(strings.Join(t[1:], " "))
+		yr := t[0]
+		tms = append(tms, bbref.NewTeam(name, yr))
+	}
+	return tms, nil
+}
+
+// TeamSetUp -> Runs functions necessary to set the
+// lineups and rotations for each team we scraped
+func TeamSetUp(tms []*bbref.Team) {
+	for _, tm := range tms {
+		tm.EstimateRotation()
+		tm.EstimateLineup()
+	}
 }
